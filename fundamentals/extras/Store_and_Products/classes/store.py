e5d7811b484ce2dcb63265e9fa8c3cc447e748fd
Naomi Glen <naomi.eve.g@gmail.com>
2023-04-10 21:08:32 -0400

    4.10.23.2

--- fundamentals/extras/Store_and_Products/classes/store.py
+++
@@ -1,9 +0,0 @@
-class Store:
-    def __init__(self, name, location):
-        self.name = name
-        self.location = location
-
-    def add_product(self,new_product):
-        pass
-    def sell_product(self, id):
-        pass
--- fundamentals/extras/Store_and_Products/store_and_products.py
+++ fundamentals/extras/Store_and_Products/store_and_products.py
@@ -1,7 +1,36 @@
-from classes.product import Product
-from classes.store import Store
+class Store:
+    def __init__(self, name, products):
+        self.name = name
+        self.products = products
+        products = []
 
-aldis = Store("Aldi's", "Lindenhurst")
-banana = Product("Banana", "Produce", "2.99/lb")
-milk = Product("Whole Milk", "Dairy", "$4.36/gal")
-porterhouse = Product("Porterhouse Steak", "Meat", "$32.99/lb")
+    def add_product(self,new_product):
+        pass
+    def sell_product(self,id):
+        pass
+    def inflation(self,percent_increase):
+        pass
+    def set_clearance(self,category,percent_discout):
+        pass
+
+class Product:
+    def __init__(self, product_name, price, category):
+        self.product_name = product_name
+        self.price = price
+        self.category = category
+
+    def update_price(self, percent_change,is_increased):
+        pass
+    def print_info(self):
+        print(f'Product:{self.product_name}, Cost:${self.price}, Category:{self.category}')
+
+bananas = Product("Bananas", 2.99, "Produce")
+bananas.print_info()
+
+milk = Product("Milk", 4.59,"Dairy")
+milk.print_info()
+
+cereal = Product("Cereal", 3.99, "Grocery")
+cereal.print_info()
+
+chicken = Product("Whole Chicken", 6.99,"Poultry")
