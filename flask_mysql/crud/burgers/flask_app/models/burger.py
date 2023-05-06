75ed3c608847813f6cfd741b7e64520572f7f74b
Naomi Glen <naomi.eve.g@gmail.com>
2023-05-06 12:32:42 -0400

    5.6.23.1

--- flask_mysql/crud/burgers/flask_app/models/burger.py
+++ flask_mysql/crud/burgers/flask_app/models/burger.py
@@ -1,18 +1,21 @@
 from flask_app.config.mysqlconnection import connectToMySQL
 
 class Burger:
-    def __init__(self,data):
-        self.id = data['id']
-        self.name= data['name']
-        self.bun = data['bun']
-        self.meat = data['meat']
-        self.calories = data['calories']
-        self.created_at = data['created_at']
-        self.updated_at = data['updated_at']
+    def __init__(self,db_data):
+        self.id = db_data['id']
+        self.name= db_data['name']
+        self.bun = db_data['bun']
+        self.meat = db_data['meat']
+        self.calories = db_data['calories']
+        self.created_at = db_data['created_at']
+        self.updated_at = db_data['updated_at']
 
     @classmethod
     def save(cls,data):
-        query = "INSERT INTO burgers (name,bun,meat,calories,created_at,updated_at) VALUES (%(name)s,%(bun)s,%(meat)s,%(calories)s,NOW(),NOW())"
+        query = """
+                INSERT INTO burgers (name,bun,meat,calories,created_at,updated_at) 
+                 VALUES (%(name)s,%(bun)s,%(meat)s,%(calories)s,%(restaurant_id)s,NOW(),NOW())
+                """
         return connectToMySQL('burgers').query_db(query,data)
 
     @classmethod
